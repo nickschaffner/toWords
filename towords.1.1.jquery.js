49b7b3d3fb91867a480120b58a816e56c6e276fb
Ricardo Andrés Bello <ricardoaandres@me.com>
2014-01-17 21:10:35 -0300

    Refactor replaced tabs for spaces

--- towords.1.1.jquery.js
+++ towords.1.1.jquery.js
@@ -1,146 +1,146 @@
 /*
-*	toWords jQuery Plugin v0.2
+* toWords jQuery Plugin v0.2
 *
-*	author: Ricardo Andrés Bello (@ricardoaandres)
-*	
-*	first release: May, 2012
+* author: Ricardo Andrés Bello (@ricardoaandres)
+* 
+* first release: May, 2012
 */
 
 
 
 if ( typeof Object.create !== 'function' ){
-	Object.create = function( obj ){
-		function F(){};
+  Object.create = function( obj ){
+    function F(){};
 
-		F.prototype = obj;
+    F.prototype = obj;
 
-		return new F();
-	}
+    return new F();
+  }
 }
 
 (function( $, undefined ){
 
-	var toWords = {
+  var toWords = {
 
-		init: function( node, config ){
-			this.elem = node;
-			this.$elem = $( this.elem );
-			this.number = this.$elem.text().replace( /[^0-9]/g, '' ).split( '' );
-			this.stringed = '';
+    init: function( node, config ){
+      this.elem = node;
+      this.$elem = $( this.elem );
+      this.number = this.$elem.text().replace( /[^0-9]/g, '' ).split( '' );
+      this.stringed = '';
 
-			this.updateSettings( config );
+      this.updateSettings( config );
 
-			this.createString( this.number );
-			this.appendString();
+      this.createString( this.number );
+      this.appendString();
 
-			this.callback();
-		},
+      this.callback();
+    },
 
-		createString: function( n ){
-			var stringed = '',
-				times = n.length,
-				zeros = '';
+    createString: function( n ){
+      var stringed = '',
+        times = n.length,
+        zeros = '';
 
-			if ( times <= 9 ){
-				for ( var cntr = 9 - times; cntr !== 0; cntr-- ){
-					zeros += '0';
-				}
+      if ( times <= 9 ){
+        for ( var cntr = 9 - times; cntr !== 0; cntr-- ){
+          zeros += '0';
+        }
 
-				if ( this.config.reverse ){
-					n = n.reverse().join( '' );
-					n = ( zeros + n ).split( '' );
-				}else{
-					n = zeros + n.join( '' );
-					n = n.split( '' );
-				}
+        if ( this.config.reverse ){
+          n = n.reverse().join( '' );
+          n = ( zeros + n ).split( '' );
+        }else{
+          n = zeros + n.join( '' );
+          n = n.split( '' );
+        }
 
-				stringed += this.stringNumber( n[0], n[1], n[2] );
-				stringed += ( n[0] == 0 && n[1] == 0 && n[2] == 0 ) 
-					? '' 
-					: ' million ';
-				stringed += this.stringNumber( n[3], n[4], n[5] );
-				stringed += ( n[3] == 0 && n[4] == 0 && n[5] == 0 ) 
-					? '' 
-					: ' thousand ';
-				stringed += this.stringNumber( n[6], n[7], n[8] );
-			}else{
-				stringed = 'Max number is 999,999,999';
-			}	
+        stringed += this.stringNumber( n[0], n[1], n[2] );
+        stringed += ( n[0] == 0 && n[1] == 0 && n[2] == 0 ) 
+          ? '' 
+          : ' million ';
+        stringed += this.stringNumber( n[3], n[4], n[5] );
+        stringed += ( n[3] == 0 && n[4] == 0 && n[5] == 0 ) 
+          ? '' 
+          : ' thousand ';
+        stringed += this.stringNumber( n[6], n[7], n[8] );
+      }else{
+        stringed = 'Max number is 999,999,999';
+      } 
 
-			this.stringed = ( stringed == '' ) ? 'zero' : stringed;
-		},
+      this.stringed = ( stringed == '' ) ? 'zero' : stringed;
+    },
 
-		stringNumber: function( numb1, numb2, numb3 ){
-			var unit = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
-				tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
-				decs = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    stringNumber: function( numb1, numb2, numb3 ){
+      var unit = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+        tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+        decs = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-			if ( numb1 != 0 ){
-				numb1 = unit[numb1] + ' hundred ';
-			}else{
-				numb1 = '';
-			}
+      if ( numb1 != 0 ){
+        numb1 = unit[numb1] + ' hundred ';
+      }else{
+        numb1 = '';
+      }
 
-			if ( numb2 != 0 ){
-				if ( numb2 == 1 ){
-					numb2 = tens[numb3];
-					numb3 = '';
-				}else{
-					numb2 = decs[numb2];
+      if ( numb2 != 0 ){
+        if ( numb2 == 1 ){
+          numb2 = tens[numb3];
+          numb3 = '';
+        }else{
+          numb2 = decs[numb2];
 
-					if ( numb3 != 0 ){
-						numb3 = '-' + unit[numb3];
-					}else{
-						numb3 = '';
-					}
-				}
-			}else{
-				numb2 = '';
+          if ( numb3 != 0 ){
+            numb3 = '-' + unit[numb3];
+          }else{
+            numb3 = '';
+          }
+        }
+      }else{
+        numb2 = '';
 
-				if ( numb3 != 0 ){
-					numb3 = unit[numb3];
-				}else{
-					numb3 = '';
-				}
-			}
+        if ( numb3 != 0 ){
+          numb3 = unit[numb3];
+        }else{
+          numb3 = '';
+        }
+      }
 
-			return ( numb1 + numb2 + numb3 == 0 ) ? '' : numb1 + numb2 + numb3;
-		},
+      return ( numb1 + numb2 + numb3 == 0 ) ? '' : numb1 + numb2 + numb3;
+    },
 
-		appendString: function(){
-			var destination = this.config.destination;
+    appendString: function(){
+      var destination = this.config.destination;
 
-			if ( !destination ){
-				destination = this.$elem;
-			}
+      if ( !destination ){
+        destination = this.$elem;
+      }
 
-			$( destination ).text( this.stringed );
-		},
+      $( destination ).text( this.stringed );
+    },
 
-		updateSettings: function( config ){
-			this.config = $.extend( {}, $.fn.toWords.config, config );
-		},
+    updateSettings: function( config ){
+      this.config = $.extend( {}, $.fn.toWords.config, config );
+    },
 
-		callback: function(){
-			this.config.callback();
-		}
+    callback: function(){
+      this.config.callback();
+    }
 
-	}
+  }
 
-	$.fn.toWords = function( config ){
+  $.fn.toWords = function( config ){
 
-		return this.each( function(){
-			var sl = Object.create( toWords );
+    return this.each( function(){
+      var sl = Object.create( toWords );
 
-			sl.init( this, config );
-		});
-		
-	}
+      sl.init( this, config );
+    });
+    
+  }
 
-	$.fn.toWords.config = {
-		reverse: false,
-		callback: function(){}
-	}
+  $.fn.toWords.config = {
+    reverse: false,
+    callback: function(){}
+  }
 
 })( jQuery );
 
